c89725cfbdb3655f52d686e488cac73203b9b8b5
eddywithay <edmundalyn@gmail.com>
2019-07-10 13:23:39 -0400

    add drill for page 100

--- Nice Mean Game/Nice Mean Game/Nice_Mean_Game.py
+++ Nice Mean Game/Nice Mean Game/Nice_Mean_Game.py
@@ -1,1 +1,39 @@
+#
+# Python:   3.7
+#
+# Author:   Edmund Alyn Jones
+#
+# Purpose:  The Tech Academy - Python Course, Creating our first program together.
+#           Demonstratin how to pass variables from function to function.
+#           while producing a functional game.
+#
+#           Remember, myFunction(myVariable) means we add the variable.
+#           Return variable implies we are calling the variable back to the invoked function.
 
+
+def start(nice=0,mean=0,nice=""):
+    #get user's name
+    #this line establishes the var called name and sets it equal to the function describeGame
+    name = describeGame(name)
+    #here we have three variables set up in conjunction with the function called niceMean
+    nice,mean,name = niceMean(nice,mean,name)
+
+def describeGame(name):
+    """
+        Check if this is a new game or not.
+        If it is new, get the user's name.
+        If it is not a new game, thank the player for
+        playing agian and continue the game.
+    """
+
+
+
+ 
+
+
+
+
+
+
+if __name__ == "__main__":
+    start()
---
+++ PyDrills/drillPage100.py
@@ -0,0 +1,59 @@
+#	Author:		Edmund Alyn Jones
+#
+#	Purpose:	This scripts job is to check a specific folder on the hard drive
+#				and verify whether a file ends with '.txt' file extension. If it does
+#				the script should print the qualifying file names and time-stamps to the console for the user.
+#	
+#   Note:       I will create a directory with at least two 'txt' files to see if this script actually works.
+#
+#	File:		drillPage100.py
+#
+#	Project:	GetThemTxtsYo!
+
+#import os module
+import os
+
+#use the listdir() method to iterate through files
+
+#first we need to establish variables/containers for the path and the files
+
+pathName = 'C:\\PyProjects\\'
+
+userListDir = os.listdir(pathName)
+
+
+
+def getDir():
+
+
+    for file in userListDir:
+
+        if '.txt' in file:
+        
+            #path.join to concat the file name to its file path. This allows us to print an absolute path to the console
+            
+            fullPath = os.path.join(pathName,file)
+            
+            #getmtime() is the method that will help us retrieve the timestamp for each file.
+            
+            timeStp = os.path.getmtime(fullPath)
+
+            #Remember you need the script to print to the console the name of the 'txt' files AND their respective timestamps.
+
+            print("The path is: " + fullPath + "\nThe last modification time is: " + str(timeStp))
+
+        else:
+            print('This file does not have the \'.txt\' designation.')
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    getDir()
